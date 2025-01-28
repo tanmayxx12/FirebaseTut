f8b8b39b2fefa48185f37e82ae0a780594c5a3c2
tanmayxx12 <troy12rollsza@gmail.com>
2025-01-28 13:50:11 +0530

    [Launch] Configured Firebase to XCode

--- FirebaseTut/FirebaseTutApp.swift
+++ FirebaseTut/FirebaseTutApp.swift
@@ -5,10 +5,17 @@
 //  Created by Tanmay . on 28/01/25.
 //
 
+import Firebase
 import SwiftUI
 
 @main
 struct FirebaseTutApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+        print("Configured Firebase")
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
