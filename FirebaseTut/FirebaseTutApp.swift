2526c4e670385cb42259cccdbc2c2d518d670cd5
tanmayxx12 <troy12rollsza@gmail.com>
2025-01-28 13:23:00 +0530

    Initial Commit

---
+++ FirebaseTut/FirebaseTutApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTutApp.swift
+//  FirebaseTut
+//
+//  Created by Tanmay . on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
